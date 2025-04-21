bc5a39052ca35e94ca1c6866bf3b375d506cbc82
ahmad <aae22138@gmail.com>
2025-04-21 16:05:34 +0200

    refactor: remove server startup code and export app directly
    The server startup code was removed from the entry point file to decouple the application instance from the server configuration. This allows for more flexible usage of the app instance, such as in testing or different deployment scenarios.

--- dist/index.js
+++ dist/index.js
@@ -4,6 +4,4 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = __importDefault(require("./app"));
-app_1.default.listen(3000, () => {
-    console.log('Server is running on port 3000');
-});
+exports.default = app_1.default;
